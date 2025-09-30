2061458e70ff8bd3b8f4edf701cbdbaf7790a09f
felipelundgren-bsatech <felipe.lundgren@bsatech.io>
2025-09-30 14:41:27 -0300

    A: inicio do projeto

---
+++ Musica.cs
@@ -0,0 +1,32 @@
+﻿class Musica
+{
+    public string Nome {  get; set; }
+    public string Artista {  get; set; }
+    public int Duracao {  get; set; }
+    public bool Disponivel {  get; set; }
+
+    public string DescricaoResumida 
+    { 
+        get
+        {
+            return $"A musica {Nome} pertence a banda {Artista}";
+        }
+    }
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Música {Nome}");
+        Console.WriteLine($"Artista:{Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponivel no plano");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano+");
+        }
+        Console.WriteLine(DescricaoResumida);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+Musica musica1 = new Musica();
+musica1.Nome = "Roxane";
+musica1.Artista = "The police";
+musica1.Duracao = 273;
+musica1.Disponivel = true;
+
+
+
+
+
+Musica musica2 = new Musica();
+musica2.Nome = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracao = 333;
+musica2.Disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
