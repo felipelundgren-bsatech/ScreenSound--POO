e7437ec0abb5bce00527a31722ea2c1f634556e4
felipelundgren-bsatech <felipe.lundgren@bsatech.io>
2025-09-30 15:28:56 -0300

    E: Criaçao da classe Banda

--- Musica.cs
+++ Musica.cs
@@ -4,6 +4,8 @@
     public string Artista {  get; set; }
     public int Duracao {  get; set; }
     public bool Disponivel {  get; set; }
+    
+    public Genero Genero { get; set; }
 
     public string DescricaoResumida =>
         $"A musica {Nome} pertence a banda {Artista}";
--- Program.cs
+++ Program.cs
@@ -3,9 +3,17 @@ albumDoQUeen.Nome = "A night at the opera";
 
 Musica musica1 =new Musica();
 musica1.Nome = "Love of my life";
+musica1.Duracao = 213;
 
 Musica musica2 =new Musica();
 musica2.Nome = "Bohemia Rhapsody";
+musica2.Duracao = 354;
+
 
 albumDoQUeen.AdicionarMusica(musica1);
 albumDoQUeen.AdicionarMusica(musica2);
+
+Banda queen = new Banda();  
+queen.Nome = "Queen";
+queen.AdicionarAlbum(albumDoQUeen);
+queen.ExibirDiscografia();
